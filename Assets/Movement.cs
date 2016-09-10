c16654b484a666e20ecde4dcd2d7719e5c4976cd
Ibrahim Abouzied <Ibrahim Abouzied>
2016-09-10 16:07:23 -0500

    The Balloon

---
+++ Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    //Variables for rotation
+    public float SpeedH = 4.0f;
+    public float SpeedV = 4.0f;
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TheMovement();
+	}
+
+    void TheMovement()
+    {
+        //Rotating the camera
+        yaw += SpeedH * Input.GetAxis("Mouse X");
+        pitch -= SpeedH * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
---
+++ Assets/MovementBalloon.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementBalloon : MonoBehaviour {
+    new Rigidbody rb;
+    
+    //For Changing speed of movement.
+    public float movSpeed = 10.0F;
+
+    //Vertical Movement for 'space'
+    public float jumpPower = 0.2F;
+
+    //Variables for rotation
+    public float SpeedH = 4.0f;
+    public float SpeedV = 4.0f;
+    public float yaw = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TheMovement();
+    }
+
+    void TheMovement()
+    {
+        //Moving the balloon across the axe's
+        float horMovement = Input.GetAxis("Horizontal") * movSpeed * Time.deltaTime;
+        float verMovement = Input.GetAxis("Vertical") * movSpeed * Time.deltaTime;
+
+        //Rotating balloon, only horizontal rotation
+        yaw += SpeedH * Input.GetAxis("Mouse X");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+
+
+        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+        transform.Translate(horMovement, 0, verMovement);
+    }
+}
