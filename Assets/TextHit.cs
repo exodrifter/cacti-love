c665330a0952de4344fb4106f8e0f3202bb01415
Arthur-Hinsvark <ajhinsvark@gmail.com>
2016-09-11 08:28:37 -0500

    Text messaging capabilities

---
+++ Assets/TextHit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextHit : MonoBehaviour {
+
+    public GameObject[] library;
+
+    void OnCollisionEnter(Collision col)
+    {
+        SpawnText();
+    }
+
+    // Use this for initialization
+    void SpawnText () {
+        GameObject text = (GameObject) Instantiate(library[Random.Range(0, library.Length)], transform.position, Quaternion.identity);
+        TextMesh tm = text.GetComponent<TextMesh>();
+        tm.color = Random.ColorHSV();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/TextLife.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextLife : MonoBehaviour {
+
+    float lifeSpan = 3f;
+    float elapsedTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.up * Time.deltaTime * 3f + transform.right * Mathf.Sin(elapsedTime*2f) * Time.deltaTime;
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime >= lifeSpan)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
