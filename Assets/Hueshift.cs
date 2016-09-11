af4ee9bb7a7c02f16a7111ab2ff44653a5ebc1f2
Ava Pek <exodrifter@gmail.com>
2016-09-11 05:31:10 -0500

    Add Game Timer

--- Assets/Hueshift.cs
+++ Assets/Hueshift.cs
@@ -5,7 +5,7 @@ public class Hueshift : MonoBehaviour {
 
 	void Update () {
 		var text = GetComponent<Text>();
-		var h = Mathf.Sin(Time.timeSinceLevelLoad);
+		var h = (Mathf.Sin(Time.timeSinceLevelLoad) + 1) / 2f;
 		text.color = new HSBColor(h, 1, 1, 1).ToColor();
 	}
 }
