4812f9c7f58b4c1c66be79e9f578e6a575ea1dcc
Ava Pek <exodrifter@gmail.com>
2016-09-10 20:25:20 -0500

    Add shutdown sound

--- Assets/Scripts/BalloonDeath.cs
+++ Assets/Scripts/BalloonDeath.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class BalloonDeath : MonoBehaviour {
 
+	public AudioSource source;
+	public AudioSource music;
+
 	// collision detection with cactus
 	void OnCollisionEnter(Collision col) {
 		if (col.other.tag == "Cactus") {
 			Camera.main.transform.parent = null;
 			Destroy(this.gameObject);
+
+			source.transform.position = transform.position;
+			source.Play();
+			music.Stop();
 			/*
 			MeshRenderer balloonMesh = GetComponentInChildren<MeshRenderer>();
 			balloonMesh.enabled = false;
