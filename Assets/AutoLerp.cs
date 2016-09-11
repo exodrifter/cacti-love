ea4ff722fcb6eeea57d0bf9a31355d3b90c0f0ed
Ava Pek <exodrifter@gmail.com>
2016-09-11 01:12:02 -0500

    add initial pyramid

---
+++ Assets/AutoLerp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoLerp : MonoBehaviour {
+
+	public Vector3 offset;
+	private Vector3 start;
+
+	public float time;
+
+	void Start ()
+	{
+		start = transform.position;
+		StartCoroutine(Move());
+	}
+
+	IEnumerator Move()
+	{
+		float elapsed = 0;
+
+		var begin = start + offset;
+		while (elapsed < time) {
+			var t = elapsed / time;
+			transform.position = Vector3.Lerp(begin, start, t);
+
+			yield return new WaitForEndOfFrame();
+			elapsed += Time.deltaTime;
+		}
+
+		transform.position = start;
+	}
+}
